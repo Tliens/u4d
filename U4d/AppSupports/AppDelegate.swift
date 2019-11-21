34366865c6761662e071fc95ba60f35a81075702
宇宙无敌天下第一魁君主上 <maninios@163.com>
2019-11-21 19:15:40 +0800

    init

---
+++ U4d/AppSupports/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  U4d
+//
+//  Created by Quinn Von on 2019/11/21.
+//  Copyright © 2019 Quinn Von. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
